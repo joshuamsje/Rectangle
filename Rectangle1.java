59310891c025552e7fff31c54f3fd7e4c6cc6bcf
joshuamsje <49621706+joshuamsje@users.noreply.github.com>
2020-05-28 19:06:12 -0400

    Create Rectangle1.java

---
+++ Rectangle1.java
@@ -0,0 +1,51 @@
+package group1;
+/**
+ * On my honor:
+
+ * 
+ * - I have not used source code obtained from another student,
+ * or any other unauthorized source, either modified or
+ * unmodified.
+ * 
+ * -All source code and documentation used in my program is
+ * either my original work, or was derived by me from the
+ * source code published in the textbook for this course.
+ * 
+ * - I have not discussed coding details about this project with
+ * anyone other than my parter (in case of a joint
+ * submission), instructor, ACM/UPE tutors, or the TAs assigned
+ * to this course. I understand that I may discuss the concepts
+ * of this program with other students, and that another student
+ * may help me debug my program so long as neither of us writes
+ * anything during the discussion or modifies any computer file
+ * during the discussion. I have violated neither the spirit nor
+ * letter of this restriction.
+ * 
+ * --Kurt Karpin (kkarp9)
+ * --Joshua Mathew (josh827)
+ */
+
+import java.io.IOException;
+
+/**
+ * This starts the RectangleReader class from the given command file and runs
+ * the program
+ * 
+ * @author Kurt Karpin (kkarp9)
+ * @author Joshua Mathew (josh827)
+ * 
+ * @version 2020.2.17
+ */
+public class Rectangle1 {
+    /**
+     * Project runner main method
+     * 
+     * @param args
+     *            main string array
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        @SuppressWarnings("unused")
+        RectangleReader runner = new RectangleReader(args[0]);
+    }
+}
